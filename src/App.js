b4e2627055e62bcb1fc88f6ac67330ca915d7539
孫呈侑 <Yoyo.C.Sun@CySun.local>
2019-06-25 13:52:06 +0800

    圖片 userfile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import UserFile from './components/UserFile'
 class App extends React.Component {
   render(){
     return (
       <div>
-        
+        <UserFile/>
       </div>
     );
   }
