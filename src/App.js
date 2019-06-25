f57cc4d9818f778fd9c68121dfd72f0d17ffeb17
sweetpotatofarm <bobby890515@gmail.com>
2019-06-25 14:15:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import UserFile from './components/UserFile'
+
+import Navbar from './component'
+import styles from './navbarscss.scss'
+
+
 class App extends React.Component {
   render(){
     return (
       <div>
+
         <UserFile/>
+        <Navbar></Navbar>
       </div>
     );
   }
---
+++ src/component.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './navbarscss.scss'
+class Navbar extends React.Component{
+    render(){
+        return (
+            <div className="Navbar">
+                <div className="NavbarTitle">GrouPotato
+                </div>
+                <div className="NavbarUser">王志強
+                </div>
+            </div>
+
+        )
+    }
+} 
+
+
+export default Navbar
